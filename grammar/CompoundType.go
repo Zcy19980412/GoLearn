849e25d73449c17c499352f7b4c224aadba0ed18
Calvin <zhaochenyang1998@gmail.com>
2025-03-06 20:04:19 +0800

    slice

--- grammar/CompoundType.go
+++ grammar/CompoundType.go
@@ -5,7 +5,49 @@ import "fmt"
 func main() {
 
 	//Array
-	testArray()
+	//testArray()
+
+	//slice
+	testSlice()
+
+}
+
+func testSlice() {
+	//init
+	var slice1 []int
+	var slice2 []int = []int{1, 2, 3}
+	var slice3 []int = make([]int, 3)
+	fmt.Println(slice1)
+	fmt.Println(slice2)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3), cap(slice3))
+
+	//update
+	slice1 = append(slice1, slice2...)
+	slice2 = append(slice2, slice3...)
+	slice3 = append(slice3, slice2...)
+	fmt.Println(slice1)
+	fmt.Println(slice2)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3), cap(slice3))
+
+	//truncation
+	s1 := []int{1, 2, 3}
+	// [)
+	s2 := s1[0:1]
+	s2 = s1[:]
+	s2 = s1[0:]
+	s2 = s1[:2]
+	fmt.Println(s2)
+	//same point!!!
+	s2[0] = 100
+	fmt.Println(s1)
+
+	//copy
+	copy1 := []int{1, 2, 3}
+	var copy2 []int = make([]int, 1, 3)
+	copy(copy2, copy1)
+	fmt.Println(copy1, copy2)
 
 }
 
