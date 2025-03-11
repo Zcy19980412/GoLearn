d9ad40227f97a6654e5acd9363ba726d9cb9264d
Calvin <zhaochenyang1998@gmail.com>
2025-03-11 17:48:04 +0800

    file read

--- grammar/file.go
+++ grammar/file.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 	//testCreate()
-	testWrite()
+	//testWrite()
+	testRead()
 }
 
 func testCreate() {
@@ -37,5 +39,44 @@ func testWrite() {
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	//seek
+	str = "seek"
+	strBytes = []byte(str)
+	fileContentEndNum, err := file.Seek(0, io.SeekEnd)
+	if err != nil {
+		fmt.Println(err)
+	}
+	_, err = file.WriteAt(strBytes, fileContentEndNum)
+	if err != nil {
+		return
+	}
+
+}
+
+func testRead() {
+	openFile, err := os.OpenFile("D://test.txt", os.O_APPEND|os.O_RDONLY, os.ModeAppend)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer openFile.Close()
+	var contents = make([]byte, 10)
+	//startNum, err := openFile.Seek(0, io.SeekStart)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//_, err = openFile.ReadAt(contents, startNum)
+	//if err != nil && err != io.EOF {
+	//	fmt.Println(err)
+	//}
+	//File的光标默认在index = 0 处
+	for {
+		_, err := openFile.Read(contents)
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(string(contents))
+	}
+	fmt.Println(string(contents))
 
 }
