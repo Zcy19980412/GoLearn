825fbd610bb3c43a2864f2d234d9ce7cafe41211
Calvin <zhaochenyang1998@gmail.com>
2025-03-25 21:14:53 +0800

    mysql

--- web/http.go
+++ web/http.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"net"
 	"net/http"
 	"os"
@@ -10,7 +12,8 @@ import (
 func main() {
 
 	//testNet()
-	testHttp()
+	//testHttp()
+	testMysql()
 
 }
 
@@ -46,4 +49,22 @@ func testHttp() {
 	})
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func testMysql() {
+	open, err := sql.Open("mysql", "root:123456@(127.0.0.1:3306)/todolist")
+	errFunc(err, "open")
+	defer open.Close()
+
+	query, err := open.Query("select * from habit limit 10")
+	errFunc(err, "query")
+	defer query.Close()
+	for query.Next() {
+		columns, _ := query.Columns()
+		var id, time, time1, userid, gapdays, name, desc, rate, imrate string
+		fmt.Println(columns)
+		err := query.Scan(&id, &time, &time, &userid, &gapdays, &name, &desc, &rate, &imrate)
+		errFunc(err, "scan")
+		fmt.Println(id, time, time1, userid, gapdays, name, desc, rate, imrate)
+	}
 }
