0c1c8835778ea839c3ef27393969722a47c0aa8e
Calvin <zhaochenyang1998@gmail.com>
2025-04-01 00:01:20 +0800

    context

--- concurrent/context.go
+++ concurrent/context.go
@@ -9,12 +9,22 @@ import (
 
 func main() {
 
-	//testContextWithValue()
-	testContextWithCancel()
+	testContextWithValue()
 
+	//context作为一个信号接收器，接收（select）其他go程的done信号
+	//testContextWithCancel()
+
+	//和Deadline一样，超过deadline直接close Context.Done()
+	//testContextTimeOut()
 }
-
-// context作为一个信号接收器，接收（select）其他go程的done信号
+func testContextWithValue() {
+	background := context.Background()
+	withValue := context.WithValue(background, "key", "value")
+	go func() {
+		fmt.Println(withValue)
+	}()
+	time.Sleep(1 * time.Second)
+}
 func testContextWithCancel() {
 	backGround := context.Background()
 	withCancel, cancelFunc := context.WithCancel(backGround)
@@ -58,4 +68,27 @@ func testContextWithCancel() {
 	time.Sleep(10 * time.Second)
 	fmt.Println("3")
 	waitGroup.Wait()
+}
+func testContextTimeOut() {
+	backGround := context.Background()
+	timeout, cancel := context.WithTimeout(backGround, 10*time.Second)
+
+	go func(timeout context.Context) {
+		for {
+			select {
+			case c := <-timeout.Done():
+				fmt.Println(c)
+				return
+			default:
+				fmt.Println("hello world")
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}(timeout)
+	time.Sleep(13 * time.Second)
+	cancel()
+	for {
+
+	}
+
 }
